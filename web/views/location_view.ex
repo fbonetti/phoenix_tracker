8d7b97cf8749df8719c1b6107c5ab7d7e62d2efe
Frank Bonetti <frank.r.bonetti@gmail.com>
2016-07-14 23:03:49 -0500

    Properly render json

--- web/views/location_view.ex
+++ web/views/location_view.ex
@@ -4,4 +4,11 @@ defmodule PhoenixTracker.LocationView do
   def render("index.json", %{locations: locations}) do
     %{data: render_many(locations, PhoenixTracker.LocationView, "location.json")}
   end
+
+  def render("location.json", %{location: location}) do
+    %{id: location.id,
+      latitude: location.latitude,
+      longitude: location.longitude,
+      recorded_at: location.recorded_at}
+  end
 end
