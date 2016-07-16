98f2f4e25ddf141b1e616e26338eb6d17774f0d6
Frank Bonetti <frank.r.bonetti@gmail.com>
2016-07-16 15:43:26 -0500

    Fix again

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :phoenix_tracker, PhoenixTracker.Endpoint,
 config :logger, level: :info
 
 # Configure your database
-config :phoenix_tracker, HelloPhoenix.Repo,
+config :phoenix_tracker, PhoenixTracker.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
